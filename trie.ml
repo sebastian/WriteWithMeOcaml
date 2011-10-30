6bf4f7c0e2fc5aff00b4196dd2e07a11994d0406
Sebastian Probst Eide <sebastian.probst.eide@gmail.com>
2011-10-30 12:24:09 +0000

    Can now predict next words

--- trie.ml
+++ trie.ml
@@ -55,30 +55,41 @@ let entry_for_character_in_new_word c ce =
   _efc c ce.new_word setter 
 let entry_for_character c ce = _efc c ce.next (fun e -> ce.next <- e)
 
+(* chop of the first word of a string *)
+let less_first_word text = 
+  try
+    let first_space = (String.index_from text 0 ' ') + 1 in
+    String.sub text first_space ((String.length text) - first_space)
+  with Not_found -> ""
+
 (* Add a string to the trie *)
-let add_text trie text =
-  let word_char_list = prep_text text in
-  let increment_word_count w = w.word_count <- w.word_count + 1 in
-  let rec traverse = function
-    (* we have reached the end of the last word *)
-    | ([], [], trie, _new_word) ->
-        (* we are at the end of a word. Count it *)
-        increment_word_count trie;
-    (* we have reached the end of a word *)
-    | ([], word::words, trie, _new_word) ->
-        (* update the count for the current word *)
-        increment_word_count trie;
-        traverse (word, words, trie, true) 
-    (* we have a word, plus additional words *)
-    | (c::cs, words, trie, false) ->
-        let e = entry_for_character c trie in traverse (cs, words, e, false)
-    (* we have a word, and the current character is the first in the word *)
-    | (c::cs, words, trie, true) ->
-        let e = entry_for_character_in_new_word c trie in traverse (cs, words, e, false) 
-  in let add_words = function
-    | ([], trie) -> traverse ([], [], trie, true)
-    | ((w::ws), trie) -> traverse (w, ws, trie, true) 
-  in add_words(word_char_list, trie)
+let rec add_text = function
+  | (trie, "") -> ()
+  | (trie, text) ->
+    let word_char_list = prep_text text in
+    let increment_word_count w = w.word_count <- w.word_count + 1 in
+    let rec traverse = function
+      (* we have reached the end of the last word *)
+      | ([], [], trie, _new_word) ->
+          (* we are at the end of a word. Count it *)
+          increment_word_count trie;
+      (* we have reached the end of a word *)
+      | ([], word::words, trie, _new_word) ->
+          (* update the count for the current word *)
+          increment_word_count trie;
+          traverse (word, words, trie, true) 
+      (* we have a word, plus additional words *)
+      | (c::cs, words, trie, false) ->
+          let e = entry_for_character c trie in traverse (cs, words, e, false)
+      (* we have a word, and the current character is the first in the word *)
+      | (c::cs, words, trie, true) ->
+          let e = entry_for_character_in_new_word c trie in traverse (cs, words, e, false) 
+    in let add_words = function
+      | ([], trie) -> traverse ([], [], trie, true)
+      | ((w::ws), trie) -> traverse (w, ws, trie, true) 
+    in 
+      add_words(word_char_list, trie);
+      add_text(trie, (less_first_word text))
 
 (* converts a list of chars to a string *)
 let string_of_chars chars =
@@ -92,12 +103,14 @@ let normalize trie =
   let rec child_finder trie word_acc curr_next curr_next_count =
     let current_word = trie.character :: word_acc in
     let children = trie.next in
-    let start_values = if trie.word_count > 0 then
+    let start_values = if (trie.word_count > 0) && (trie.word_count > curr_next_count) then
       (* We have a potential new candidate *)
       (current_word, trie.word_count)
     else (curr_next, curr_next_count) in
+    (* look through all the subsequent words *)
+    best_child trie;
+    (* find the current best word *)
     List.fold_left (fun (w, c) child ->
-      best_child child;
       let (ww, cc) = child_finder child current_word w c in
       if c > cc then (w, c) else (ww, cc)) start_values children
   and best_child element =
@@ -108,5 +121,39 @@ let normalize trie =
     ()
   in best_child trie
 
+(* returns the next word after a given string *)
+let rec next_word trie word_list =
+  let rec find_next = function
+    | (tries, []) -> ""
+    | (tries, word::words) ->
+        try let next_trie = List.find (fun e -> e.character == List.hd word) tries in
+          follow_word (next_trie, (List.tl word)::words)
+        with Not_found -> next_word trie (List.tl word_list)
+  and follow_word = function
+    (* we are at the end of the sentence *)
+    | (trie, [[]]) -> trie.best_next_word
+    (* we have reached the end of a word *)
+    | (trie, []::words) -> find_next(trie.new_word, words)
+    (* we progress through the trie *)
+    | (trie, words) -> find_next(trie.next, words)
+  in follow_word (trie, []::word_list)
+
+(* finds the next n words that follow a string of words *)
+let next_words = function
+  | (trie, "", _number) -> trie.best_next_word
+  | (trie, text, number) -> 
+      let rec finder = function
+        | (_trie, _text, new_text, 0) -> new_text
+        | (trie, text, new_text, number) ->
+            let word_char_list = prep_text text in
+            match (next_word trie (word_char_list)) with
+              | "" -> new_text
+              | "." -> finder(trie, text ^ ".", new_text ^ ".", number - 1)
+              | "," -> finder(trie, text ^ ".", new_text ^ ",", number - 1)
+              | word -> finder(trie, text ^ " " ^ word, new_text ^ " " ^ word, number - 1)
+      in finder(trie, text, "", number)
+
 (* Creates an empty trie tree *)
 let words = entry_for_character '-'
+
+(* let t = { character = '-'; best_next_word = ""; word_count = 0; child_count = 0; next = []; new_word = []; };; *)
--- trie.mli
+++ trie.mli
@@ -11,7 +11,10 @@ val prep_text : string -> char list list
 val _efc : char -> entry list -> (entry list -> 'a) -> entry
 val entry_for_character_in_new_word : char -> entry -> entry
 val entry_for_character : char -> entry -> entry
-val add_text : entry -> string -> unit
+val less_first_word : string -> string
+val add_text : entry * string -> unit
 val string_of_chars : char list -> string
 val normalize : entry -> unit
+val next_word : entry -> char list list -> string
+val next_words : entry * string * int -> string
 val words : entry -> entry
